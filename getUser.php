2645a45b1479a38b0a999689d1df21c35ecc89a9
Elessa Fermano <elessamaefermano@gmail.com>
2024-03-14 16:43:28 +0800

    third commit

--- getUser.php
+++ getUser.php
@@ -3,6 +3,6 @@
 include './migrations/get-users-table.php';
 
 $get = new GetUser();
-$data = $get->getAll();
+$data = $get->getAll($_GET);
 echo json_encode($data);
 ?>
--- migrations/get-users-table.php
+++ migrations/get-users-table.php
@@ -4,9 +4,12 @@ include './database/db.php';
 
 class GetUser extends Database
 {
-    public function getAll()
+    public function getAll($params)
     {
-        $get = $this->conn->query("SELECT * FROM users where first_name = 'elessa' ");
+        
+        $firstname = $params ['first_name'];
+
+        $get = $this->conn->query("SELECT * FROM users where first_name = '$firstname' ");
         $result = $get->fetch_all(MYSQLI_ASSOC);
         return $result;
 
