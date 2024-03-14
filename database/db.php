ccd0fd5d0483f070b9bd4889a7e7e96da9572e42
Elessa Fermano <elessamaefermano@gmail.com>
2024-03-14 16:30:42 +0800

    second commit

--- database/db.php
+++ database/db.php
@@ -7,9 +7,9 @@ class Database
     public function __construct()
     {
         $this->conn = new mysqli("localhost","root","");
-        $db = $this->conn->query("CREATE DATABASE IF NOT EXISTS activity");
+        $db = $this->conn->query("CREATE DATABASE IF NOT EXISTS api_db");
 
-        $use = $this->conn->query("USE activity");
+        $use = $this->conn->query("USE api_db");
     }
 }
 
